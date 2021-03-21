04f58c63a59e14dd52f694b178c0056853f338d3
sadnessITS <dima346922336@gmail.com>
2021-03-21 15:00:00 +0300

    An authorization system was created. You must delete the existing user table.

--- admin/start.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    require_once "functions.php";
-    if (!((checkUser($_SESSION["email"], $_SESSION["password"])) && (isAdmin($_SESSION["email"])))) {
-        header ("Location: /admin/auth.php");
-    }
-?>
